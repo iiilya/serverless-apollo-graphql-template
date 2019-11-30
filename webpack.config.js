da6721257bada5205c3f65a166752b3a0a9aec58
Ilya Ivanov <ilyaivanov765@gmail.com>
2019-11-30 10:38:15 +0100

    Update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
   target: "node",
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: "ts-loader" }
     ]
   }
